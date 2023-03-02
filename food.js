b9a13f286341a31434ad51fa1997ed1c45f77dca
CharlesS6 <90624368+CharlieScott4@users.noreply.github.com>
2023-03-02 14:55:46 +0000

    Initial commit

--- food.js
+++ food.js
@@ -1,3 +1,5 @@
+var mySound;
+
 import { onSnake, expandSnake } from './snake.js'
 import { randomGridPosition } from './grid.js'
 
@@ -5,10 +7,19 @@ let food = getRandomFoodPosition()
 const EXPANSION_RATE = 5
 
 export function update() {
+
   if (onSnake(food)) {
     expandSnake(EXPANSION_RATE)
     food = getRandomFoodPosition()
+    playSound();
   }
+}
+
+function playSound() {
+  console.log("Play sound");
+  let mySound = new sound("music/cow.mp3");
+  mySound.play();
+
 }
 
 export function draw(gameBoard) {
@@ -26,3 +37,21 @@ function getRandomFoodPosition() {
   }
   return newFoodPosition
 }
+
+// Sound consructor
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+
+  }
+  this.stop = function() {
+    this.sound.pause();
+  }
+}
+
--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+var mySound2;
+
+
+
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
@@ -8,11 +12,19 @@ const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
   if (gameOver) {
-    if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
-    }
+         playSound2();
+    const myTimeout = setTimeout(gameOverAlert, 2000);
+   
+
     return
   }
+
+  function gameOverAlert()
+    {
+      if (confirm('You lost. Press ok to restart.')) {
+       window.location = '/'
+      }
+    }
 
 
   window.requestAnimationFrame(main)
@@ -42,4 +54,28 @@ function draw() {
 
 function checkDeath() {
   gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+ 
+}
+function playSound2() {
+   console.log("Play sound");
+   let mySound2 = new sound("music/bomb.mp3");
+   mySound2.play();
+
+}
+
+// Sound consructor
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function() {
+    this.sound.play();
+
+  }
+  this.stop = function() {
+    this.sound.pause();
+  }
 }
